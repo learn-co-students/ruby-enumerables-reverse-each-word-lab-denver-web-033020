9828d0ac8104c32a22f4413383cf900d5e3f35b8
Daniel Steed <dsteed112@gmail.com>
2020-03-18 21:02:11 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#difference between a .each and .collect
+
+def reverse_each_word(string)
+   string = string.split(" ")
+    #i = 0
+    #j = 0
+    #newString = [" "]
+    #while(i < string.length)
+    #string.each do |string|
+     #string = string.reverse()
+     #newString[j] = string
+       #i += 1
+       #j += 1
+    # end
+      newString = string.collect{|reverse_string| reverse_string.reverse}
+  #end
+  return newString.join(' ')
+end
